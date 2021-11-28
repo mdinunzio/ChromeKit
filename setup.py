da31e083a5017c4209e1060e787de81fcb41262a
Michael DiNunzio <mdinunzio@gmail.com>
2021-11-27 20:19:42 -0500

    1.1.2: Added package spec

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="1.1.0",
     author="Michael DiNunzio",
     author_email="mdinunzio@gmail.com",
+    packages=['chromekit'],
     description="A utility for easily maintaining and accessing ChromeDriver.",
     keywords="ChromeDriver Selenium WebDriver",
     url="https://github.com/mdinunzio/ChromeKit",
